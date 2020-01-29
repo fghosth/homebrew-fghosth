191a5c44a841b3c14c08eab91bbe1128c46da70e
derek fan <fghosth@163.com>
2020-01-29 13:35:48 +0800

    feat: rb

---
+++ Formula/hello.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Hello < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/fghosth/homebrew-hex/releases/download/v1.0/hello.tar.gz"
+  sha256 "bdac0658e08466c9b12565f21ad5bd9ae224b3b9eba25cfc162269f06fd0207f"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "cmake", ".", *std_cmake_args
+    bin.install "hello"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-hex`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
