0efff618aff101d04c84e3f78f9658b06bedff41
derek fan <fghosth@163.com>
2020-01-31 13:41:44 +0800

    feat: add peep

---
+++ Formula/peep.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class peep < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/fghosth/goProject/releases/download/v1.0/peep.tar.gz"
+  sha256 "6aa2c3fb7872ea192a448c756ba2d7c1a96ecd16af333b43f25f1c56a19348f3"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    bin.install "hello"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test goProject`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
